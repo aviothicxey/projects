adc8ffdce0e0b2e998f3d6d568792867dd645cf9
Aparna <gargaparna78@gmail.com>
2026-03-01 20:07:45 +0530

    init

--- Authentication/backend/utils/generateTokenAndSetCookie.js
+++ Authentication/backend/utils/generateTokenAndSetCookie.js
@@ -7,5 +7,8 @@ const generateTokenAndSetCookie = (res, userId) => {
         HTTPOnly: true, // prevents attacks like XSS
         secure: process.env.NODE_ENV === "production", // ensures cookie is sent over HTTPS in production
         sameSite: "strict", // helps prevent CSRF attacks
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     })
 }
+
+module.exports = generateTokenAndSetCookie;
