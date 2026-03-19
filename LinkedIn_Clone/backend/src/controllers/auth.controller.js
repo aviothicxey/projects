0490019a12b33aaead7e8fd940bcde011e0134b0
Aparna <gargaparna78@gmail.com>
2026-03-19 15:15:03 +0530

    init

--- LinkedIn_Clone/backend/src/controllers/auth.controller.js
+++ LinkedIn_Clone/backend/src/controllers/auth.controller.js
@@ -9,14 +9,14 @@ async function signUp(req , res , next){
     // a request comes in --> Validate --> talk to db --> send the response back to client
     try{
         // Validation errors:
-        const errors = validatrionResult(req);
+        const errors = validationResult(req);
         if(!errors.isEmpty()){
             return res.status(400).json({
                 success : false,
                 message : errors.array()
             });
         }
-        const { name , email , password} = req.body;
+        const {firstName, lastName, username, email, password } = req.body;
 
         // check is user with the same email or username already exits:
         const userAlrExist = await User.findOne({email});
@@ -40,7 +40,7 @@ async function signUp(req , res , next){
         // }  // this is no more required as we are using express validator for validation
 
         // create user:
-        const newUser = await user.create({
+        const newUser = await User.create({
             firstName,
             lastName,
             username,
@@ -48,7 +48,7 @@ async function signUp(req , res , next){
             password // this password will be hashed in the user model before saving to db
         })
 
-        const token = generateToken(user._id);
+        const token = generateToken(User._id);
         res.cookie("token" , token,{
             httpOnly : true,
             maxAge : 24 * 60 * 60 * 1000 ,// 1 dayyy
@@ -60,7 +60,7 @@ async function signUp(req , res , next){
         return res.status(201).json({
             success : true,
             message : "User registered successfully",
-            user 
+            User 
         })
     }catch(err){
         next(err);
--- LinkedIn_Clone/backend/src/routes/auth.route.js
+++ LinkedIn_Clone/backend/src/routes/auth.route.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const authRouter = express.Router();
 const {signUp} = require('../controllers/auth.controller');
+const {body} = require('express-validator');
 
-authRouter.post('/signup' , signUp);
+authRouter.post('/signup' ,[
+    // validation using express validator:
+    body('firstName').notEmpty().withMessage("First name is required"),
+    body('lastName').notEmpty().withMessage("Last name is required"),
+    body('username').notEmpty().withMessage("Username is required"),
+    body('email').isEmail().withMessage("Please provide a valid email address"),
+    body('password').isLength({min : 6}).withMessage("Password must be at least 6 characters long")
+] , signUp);
 
 
 
